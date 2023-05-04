abb757e36f07e9a958a0395b65bc96e8386510b0
davidetaribelli <davide.taribelli@gmail.com>
2023-05-04 16:33:04 +0200

    MILESTONE 2

--- main.js
+++ main.js
@@ -55,3 +55,61 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+const containerEl = document.getElementById("container");
+
+
+
+posts.forEach((element, i, array) =>{
+
+    const singlePost = posts[i];
+    
+    let post = document.createElement("div");
+    post.classList.add("post");
+
+    post.innerHTML +=   `<div class="post__header">
+                            <div class="post-meta">
+                                <div class="post-meta__icon">
+                                    <img class="profile-pic" src="${singlePost.author.image}" alt="${singlePost.author.name}">                  
+                                </div>
+                                <div class="post-meta__data">
+                                    <div class="post-meta__author">${singlePost.author.name}</div>
+                                    <div class="post-meta__time">${singlePost.created}</div>
+                                </div>
+                            </div>
+                        </div>`;
+ 
+    post.innerHTML +=   `<div class="post__text">${singlePost.content}</div>`
+    post.innerHTML +=   `<div class="post__image">
+                            <img src="${singlePost.media}" alt="${singlePost.name}">
+                        </div>`
+
+    post.innerHTML +=   `<div class="post__footer">
+                            <div class="likes js-likes">
+                                <div class="likes__cta">
+                                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                        <span class="like-button__label">Mi Piace</span>
+                                    </a>
+                                </div>
+                                <div class="likes__counter">
+                                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                                </div>
+                            </div>
+                        </div>`;
+   
+    containerEl.append(post);
+    
+});
+
+
+
+
+
+
+
+
+
+
+
